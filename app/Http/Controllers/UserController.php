dcaebba130fb019591862c8c19f3a10fbe45d1ee
sweta <sweta.bhandari@pokersportsleague.com>
2019-09-13 17:24:32 +0530

    static otp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -208,7 +208,8 @@ class UserController extends Controller
         $apiKey = urlencode('hMkQfydUC6M-JRvPew5uwgT75vdyitJKmfztDmvSgN');
             // $resp['errorcode'] = 0;
              // Message details
-        $otp = rand(100000,999999);
+        //$otp = rand(100000,999999);
+        $otp = 111111;
         //$numbers = array(919773486995);
         $sender = urlencode('TXTLCL');
         $message = rawurlencode('This is your otp - '.$otp.' .Please put this to verify');
